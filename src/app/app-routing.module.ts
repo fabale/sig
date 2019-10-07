9d7f10aaf08a57477501cab69637b384c69b5870
fabale <alean.sg@gmail.com>
2019-10-06 22:15:12 -0500

    Actualizacion de app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,17 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+  },
+  { path: 'login', loadChildren: './sig/login/login.module#LoginPageModule' },
+  { path: 'menu', loadChildren: './sig/menu/menu.module#MenuPageModule' },
+  { path: 'calidad-h', loadChildren: './sig/calidad-h/calidad-h.module#CalidadHPageModule' },
+  { path: 'etnobotanica', loadChildren: './sig/etnobotanica/etnobotanica.module#EtnobotanicaPageModule' },
+  { path: 'chhogar', loadChildren: './sig/chhogar/chhogar.module#ChhogarPageModule' },
+  { path: 'chsincobertura', loadChildren: './sig/chsincobertura/chsincobertura.module#ChsincoberturaPageModule' },
+  { path: 'chexterior', loadChildren: './sig/chexterior/chexterior.module#ChexteriorPageModule' },
+  { path: 'etnombrecomun', loadChildren: './sig/etnombrecomun/etnombrecomun.module#EtnombrecomunPageModule' },
+  { path: 'etfoto', loadChildren: './sig/etfoto/etfoto.module#EtfotoPageModule' },
+  { path: 'etuso', loadChildren: './sig/etuso/etuso.module#EtusoPageModule' }
 ];
 @NgModule({
   imports: [
